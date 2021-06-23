6dc1025814902149b5247127edf30d587eba5e15
tpu01yzx <tpu01yzx@163.com>
2021-06-23 16:31:11 +0800

    Update AutoPlayVideo_jsglpt.js
    add auto reload function for the page to loginToStoreByAccount.

--- AutoPlayVideo_jsglpt.js
+++ AutoPlayVideo_jsglpt.js
@@ -5,21 +5,13 @@
 // @description  auto play videos!
 // @author       tpu01yzx
 // @match        https://jsglpt.gdedu.gov.cn/ncts/*
+// @require      https://jsglpt.gdedu.gov.cn/js/jquery/jquery-1.12.4.min.js
 // @icon         https://www.google.com/s2/favicons?domain=gdedu.gov.cn
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    //var ajaxHook = function() {
-    //    var $ = $ || window.$;
-    //    $.ajaxSetup({beforeSend: function(xhr, settings) {
-    //        //fix a error in updateLastViewTime for the old version.
-    //        settings.url = settings.url.replace("/nctsa_", "/ncts/a_");
-    //    }});
-    //};
-    //setTimeout(ajaxHook,1000);
 
     var main_func = function() {
         var $ = $ || window.$;
@@ -41,6 +33,38 @@
         setTimeout(main_func, 1000);
     }
     if(window.player) setTimeout(main_func,1000);
+
+    var reload_func = function() {
+        var $ = $ || window.$;
+        if($.ajaxSubmitValue == undefined) {
+            $.ajaxSubmitValue = function(urlValue) {
+                var rData = $.ajax({
+                    url: urlValue,
+                    type: "post",
+                    async: false,
+                    success: function (data) {
+                    }
+                }).responseText;
+                return rData;
+            };
+        }
+        var div0 = $('div[class="g-active-time"]');
+        if(div0 && div0.length == 1 && div0[0].innerText.indexOf("请您稍后再尝试登陆") >= 0) {
+//            window.location.reload();
+            var ticket = $.ajaxSubmitValue('/teacher/ssoItlmsTicket.action');
+            if(ticket && ticket.toString().indexOf('ITLMS-') == 0) {
+                var htmlForm = $('<form id="SSO_ITLMS_PUBLIC_STUDY_FORM" action="https://jsglpt.gdedu.gov.cn/ncts/nts/loginToStoreByAccount" method="post">  </form>');
+                htmlForm.append('<input type="hidden" id="ticket_public" name="ticket" value="' + ticket +'" />');
+                $(document.body).append(htmlForm);
+                htmlForm.submit();
+            } else {
+                setTimeout(reload_func,5000);
+            }
+        }
+    };
+    if(window.location.href == "https://jsglpt.gdedu.gov.cn/ncts/nts/loginToStoreByAccount") {
+       setTimeout(reload_func,5000);
+    }
 
     // Your code here...
 })();
